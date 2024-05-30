e6ace4bd6c399213ad572e15031e65c69e85805a
Jonathan <paternina1992@gmail.com>
2024-05-30 12:58:57 -0500

    paquetes para trabajar con JWT

--- FurnitureStore.API/obj/Debug/net8.0/FurnitureStore.API.AssemblyInfo.cs
+++ FurnitureStore.API/obj/Debug/net8.0/FurnitureStore.API.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FurnitureStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43b6e7a068212944343d5475e99537f6ee54259d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FurnitureStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FurnitureStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
