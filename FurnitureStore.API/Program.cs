dc959faf235d0a41b71ed899ec85c7b4359c3c23
Jonathan <paternina1992@gmail.com>
2024-06-18 11:03:25 -0500

    JWT + Authentication + Identity + Token + login

--- FurnitureStore.API/Program.cs
+++ FurnitureStore.API/Program.cs
@@ -1,5 +1,10 @@
+using FurnitureStore.API.Configuration;
 using FurnitureStore.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +18,34 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<FurnitureStoreContext>(options =>
             options.UseSqlite(builder.Configuration.GetConnectionString("FurnitureStoreContext")));
 
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(jwt =>
+    {
+        var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfig:Secret").Value);
+
+        jwt.SaveToken = true;
+        jwt.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false, // Para produccion debe ser verdadero
+            ValidateAudience = false, // Para produccion debe ser verdadero
+            RequireExpirationTime = false, // Para produccion debe ser verdadero
+            ValidateLifetime = true
+        };
+    });
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+        options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<FurnitureStoreContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +57,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- FurnitureStore.Data/FurnitureStoreContext.cs
+++ FurnitureStore.Data/FurnitureStoreContext.cs
@@ -1,4 +1,5 @@
 ﻿using FurnitureStore.Shared;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace FurnitureStore.Data
 {
-    public class FurnitureStoreContext : DbContext
+    public class FurnitureStoreContext : IdentityDbContext
     {
         public FurnitureStoreContext(DbContextOptions options) : base(options) { }
         public DbSet<Client> Clients { get; set; }
